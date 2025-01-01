e8e6d4d573da07760f2d0bc208256fa26eadd463
MESHEmugles <mdpeter28@gmail.com>
2025-01-02 00:24:10 +0100

    added pg online

--- core/settings.py
+++ core/settings.py
@@ -84,18 +84,22 @@ if Development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Or other database engine
-            'NAME': 'mydocumentation',
+            'NAME': 'railway',
             'USER': 'postgres',
-            'PASSWORD': 'meshe',
-            'HOST': '127.0.0.1',
+            'PASSWORD': 'tsMABeHPPctmaMNyhVZhRqOOTIFabKUI',
+            'HOST': 'postgres-3xs1.railway.internal',
             'PORT': '5432',
         }
     }
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Or other database engine
+            'NAME': 'mydocumentation',
+            'USER': 'postgres',
+            'PASSWORD': 'meshe',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
 
